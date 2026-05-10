8baaa0803dafe86a2cf376e9139a517fb93d8d67
ktechhydle <kellerhydle@gmail.com>
2026-05-10 16:56:52 -0600

    Repo Updates
    -more congfiguration and initial hardware setup

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,48 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{AnyPin, Level, Output, Pull, Speed};
-use embassy_stm32::time::khz;
-use embassy_stm32::timer::pwm_input::PwmInput;
-use embassy_stm32::{Peri, bind_interrupts, peripherals, timer};
+use embassy_stm32::Peri;
+use embassy_stm32::gpio::{AnyPin, Level, Output, Speed};
 use embassy_time::Timer;
+use steperb::StepperController;
 use {defmt_rtt as _, panic_probe as _};
-
-bind_interrupts!(struct Irqs {
-    TIM3 => timer::CaptureCompareInterruptHandler<peripherals::TIM3>;
-});
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    let mut base = StepperController::new(200);
+    base.set_desired_angle(45.0);
+
     let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
 
+    spawner.spawn(step_task(base, p.PA0.into(), p.PA1.into()).unwrap());
     spawner.spawn(blinky(p.PA5.into()).unwrap());
+}
 
-    let mut pwm_input = PwmInput::new_ch1(p.TIM3, p.PA6, Irqs, Pull::None, khz(100));
-    pwm_input.enable();
+#[embassy_executor::task]
+async fn step_task(
+    mut controller: StepperController,
+    step_pin: Peri<'static, AnyPin>,
+    dir_pin: Peri<'static, AnyPin>,
+) {
+    let mut step_pin_output = Output::new(step_pin, Level::High, Speed::Low);
+    let mut dir_pin_output = Output::new(dir_pin, Level::High, Speed::Low);
 
     loop {
-        let period_ticks = pwm_input.wait_for_period().await;
-        let period = period_ticks as f32 / 100000.0;
-        let width_ticks = pwm_input.get_width_ticks();
-        let duty_cycle = pwm_input.get_duty_cycle();
+        if controller.needs_movement() {
+            if controller.is_reversed() {
+                dir_pin_output.set_high(); // or low
+            }
 
-        info!(
-            "period: {} period ticks: {}, width ticks: {}, duty cycle: {}",
-            period, period_ticks, width_ticks, duty_cycle
-        );
+            step_pin_output.set_high();
+            Timer::after_micros(5).await;
+            step_pin_output.set_low();
+
+            controller.apply_step();
+
+            Timer::after_micros(500).await;
+        } else {
+            Timer::after_millis(1).await;
+        }
     }
 }
 
