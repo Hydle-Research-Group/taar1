01f7df35f28826b7459f9076871249ac3e4b4e0f
ktechhydle <kellerhydle@gmail.com>
2026-05-18 14:42:47 -0600

    Firmware Updates
    -fix uart communication and buffer reading

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,9 @@ bind_interrupts!(struct Irqs {
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
+    let mut uart_config = Config::default();
+    uart_config.baudrate = 115200;
+
     let mut uart = Uart::new(
         p.USART2,
         p.PA3,
@@ -44,7 +47,7 @@ async fn main(spawner: Spawner) {
         p.DMA1_CH1,
         p.DMA1_CH2,
         Irqs,
-        Config::default(),
+        uart_config,
     )
     .unwrap();
 
@@ -52,47 +55,70 @@ async fn main(spawner: Spawner) {
 
     // continuously read/write to UART
     loop {
-        let mut buffer = [0_u8; 256]; // 64 char limit
-        uart.read(&mut buffer).await.unwrap();
+        let mut buf = [0u8; 128];
+        uart.read_until_idle(&mut buf).await.unwrap();
 
-        let msg = str::from_utf8(&buffer).unwrap();
+        let mut command = heapless::Vec::<u8, 128>::new();
 
-        match parse_command(msg) {
-            Ok(cmd) => match cmd {
-                Command::MoveTo { x, y, z } => {
-                    if HOMING_ACTIVE.load(Ordering::Relaxed) {
-                        uart.write(b"Motion Error: machine is actively homing\n")
+        for &byte in &buf[..buf.len()] {
+            if byte == b'\r' {
+                continue;
+            }
+
+            // end received
+            if byte == b'\n' {
+                break;
+            }
+
+            command.push(byte).ok();
+        }
+
+        if let Ok(msg) = str::from_utf8(&command) {
+            match parse_command(msg) {
+                Ok(cmd) => match cmd {
+                    Command::MoveTo { x, y, z } => {
+                        if HOMING_ACTIVE.load(Ordering::Relaxed) {
+                            uart.write(b"Motion Error: machine is actively homing\n")
+                                .await
+                                .unwrap();
+
+                            continue;
+                        }
+
+                        let (base, arm) = solve(x, y, z);
+
+                        if (base > MAX_BASE_ROTATION || arm > MAX_ARM_ROTATION)
+                            || (base < 0.0 || arm < 0.0)
+                        {
+                            uart.write(
+                                b"Motion Error: desired position is beyond the machine limits\n",
+                            )
                             .await
                             .unwrap();
 
-                        continue;
+                            continue;
+                        }
+
+                        // join(
+                        // move_stepper_to(StepperType::Base, base, step_pin, dir_pin),
+                        // move_stepper_to(StepperType::Arm, arm, step_pin, dir_pin),
+                        // ).await;
                     }
-
-                    let (base, arm) = solve(x, y, z);
-
-                    if (base > MAX_BASE_ROTATION || arm > MAX_ARM_ROTATION)
-                        || (base < 0.0 || arm < 0.0)
-                    {
-                        uart.write(
-                            b"Motion Error: desired position is beyond the machine limits\n",
-                        )
-                        .await
-                        .unwrap();
-
-                        continue;
+                    Command::Home => {
+                        HOMING_ACTIVE.store(true, Ordering::Relaxed);
                     }
-
-                    // join(
-                    // move_stepper_to(StepperType::Base, base, step_pin, dir_pin),
-                    // move_stepper_to(StepperType::Arm, arm, step_pin, dir_pin),
-                    // ).await;
+                },
+                Err(e) => {
+                    uart.write(e.as_bytes()).await.unwrap();
+                    continue;
                 }
-                Command::Home => {
-                    HOMING_ACTIVE.store(true, Ordering::Relaxed);
-                }
-            },
-            Err(e) => uart.write(e.as_bytes()).await.unwrap(),
+            }
+        } else {
+            uart.write(b"Parse Error: invalid UTF-8\n").await.unwrap();
+            continue;
         }
+
+        uart.write(b"Command Received\n").await.unwrap();
     }
 }
 
